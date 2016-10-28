b979fb23d9836e6da9e877be74b34f9b15aa5346
Alexander Kotov <alexander_kotov@pochtamt.ru>
2016-10-28 22:12:41 +0300

    [+] index_tuple.h

---
+++ index_tuple.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstddef>
+
+template<size_t... values>
+struct IndexTuple {
+};
+
+template<class T1, class T2>
+struct IndexTupleConcat;
+
+template<size_t... v1, size_t... v2>
+struct IndexTupleConcat<IndexTuple<v1...>, IndexTuple<v2...>> {
+    using type = IndexTuple<v1..., (v2 + sizeof...(v1))...>;
+};
+
+template<size_t n>
+struct IndexTupleBuilder : IndexTupleConcat<typename IndexTupleBuilder<n / 2>::type, typename IndexTupleBuilder<n - n / 2>::type> {
+};
+
+template<>
+struct IndexTupleBuilder<1> {
+    using type = IndexTuple<0>;
+};
+
+template<>
+struct IndexTupleBuilder<0> {
+    using type = IndexTuple<>;
+};
